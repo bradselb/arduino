cfedc98a4c0e8140d254dcdc196074006c47f4e4
brad <brad@mercury>
2014-09-26 15:16:33 -0700

    add simple tcl script to get time & temp

---
+++ getTemp.tcl
@@ -0,0 +1,33 @@
+#! /usr/bin/tclsh 
+
+after 10000 [list set state timeout]
+
+proc sendRequest {f} {
+#    puts  $f {}
+    puts $f "show t"
+    flush $f
+    puts stdout "request sent"
+}
+
+proc printReply {f} {
+    global state
+    if {[gets $f reply] >= 0} {
+        puts "$reply"
+        flush stdout
+    }
+}
+
+set f [open {/dev/ttyACM0} {RDWR}]
+fconfigure $f -blocking 1 -buffering line  -encoding binary  -mode 57600,n,8,1 -timeout 10000
+# fileevent $f writable [list sendRequest $f]
+# fileevent $f readable [list printReply $f]
+
+after 2000
+sendRequest $f
+printReply $f
+printReply $f
+
+# vwait state
+# puts stdout "$state"
+
+close $f
