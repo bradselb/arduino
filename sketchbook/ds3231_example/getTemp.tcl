2ec9ff32f00a712da89cb6f6e4f0de3d30117e61
bradselb <bks62464@yahoo.com>
2014-10-04 22:33:34 -0700

    Clean up and simplify further. Works with nano.

--- sketchbook/ds3231_example/getTemp.tcl
+++ sketchbook/ds3231_example/getTemp.tcl
@@ -1,28 +1,28 @@
 #! /usr/bin/tclsh 
 
-after 10000 [list set state timeout]
+after 5000 [list set state timeout]
 
 proc sendRequest {f msg}  {
-#    puts  $f {}
     puts $f "$msg"
     flush $f
-#    puts stdout "request sent"
 }
 
 proc printReply {f} {
+    global state
     if {[gets $f reply] >= 0} {
         puts -nonewline "$reply "
         flush stdout
+        set state {done}
     }
 }
 
-set f [open {/dev/ttyACM0} {RDWR}]
+set f [open {/dev/ttyUSB0} {RDWR}]
 fconfigure $f -blocking 1 -buffering line  -encoding binary  -mode 57600,n,8,1; # -timeout 1000
 # fileevent $f writable [list sendRequest $f]
 fileevent $f readable [list printReply $f]
 
-after 2000
-sendRequest $f {show temp}
+after 500
+sendRequest $f "get temp"
 
 vwait state
 puts stdout {}
